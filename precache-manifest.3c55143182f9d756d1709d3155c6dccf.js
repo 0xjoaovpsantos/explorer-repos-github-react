c02f6aeed1cfb3c07953a0df8a5496d8acb94fe8
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-11 09:45:40 -0300

    Updates

---
+++ precache-manifest.3c55143182f9d756d1709d3155c6dccf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13960d864b45e12b2006c26553eacc02",
+    "url": "/explorer-repos-github-react/index.html"
+  },
+  {
+    "revision": "22bb6e57b5035df90ab6",
+    "url": "/explorer-repos-github-react/static/js/2.db8c428b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/explorer-repos-github-react/static/js/2.db8c428b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6124bb16d37b193d2ec3",
+    "url": "/explorer-repos-github-react/static/js/main.ffcc8fa0.chunk.js"
+  },
+  {
+    "revision": "e4e9ed1d205aaa53f437",
+    "url": "/explorer-repos-github-react/static/js/runtime-main.74f24e4a.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/explorer-repos-github-react/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/explorer-repos-github-react/static/media/logo.04f6798f.svg"
+  }
+]);
